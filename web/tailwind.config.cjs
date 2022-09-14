8c4f45d251f7f5872c27c02efe21d6c723b3e903
Pablo Alan <pabloxt14@gmail.com>
2022-09-14 19:11:32 -0300

    feat: styling inicial web page finished

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,12 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
       },
+      colors: {},
       backgroundImage: {
         galaxy: "url('background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 67.94%, #E1D55D 10.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       }
     },
   },
