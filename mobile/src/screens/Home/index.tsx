0f8615007ec5656030163c17c10c5588c67458b0
Pablo Alan <pabloxt14@gmail.com>
2022-09-16 18:34:04 -0300

    feat: implementing rest mobile

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { View, Image, ScrollView, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Image, FlatList } from 'react-native';
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
+const IP_WSL = '172.28.87.163'
+// const IP_WSL = '10.0.2.2'
+
+
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+
+  useEffect(() => {
+    fetch(`http://${IP_WSL}:3333/games`)
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <View style={styles.container}>
       <Image 
@@ -21,7 +37,7 @@ export function Home() {
       />
 
       <FlatList 
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard 
